80765cab11038a2dd2ecba9697de92a7b4df0241
easytothink <a3318114536@163.com>
2024-09-04 14:48:05 +0800

    bug修复

--- src/subjects/gen.py
+++ src/subjects/gen.py
@@ -18,12 +18,15 @@ def create_index_html(root_dir='.'):
         # 遍历当前目录下的文件  
         for filename in filenames:  
             # 排除隐藏文件（以'.'开头的文件）  
-            if not filename.startswith('.'):  
+            if '.pdf' in filename:  
                 # 生成HTML超链接，注意处理路径中的特殊字符  
                 relative_path = os.path.relpath(os.path.join(dirpath, filename), root_dir)  
                 # 使用urllib.parse.quote对路径进行编码，以避免特殊字符问题（可选）  
                 # 但在这里为了简单起见，我们假设路径不包含需要编码的字符                        
-                html_content += f"<li><a href=\"js/pdf/web/viewer.html?file=/src/subjects/{relative_path}\">{filename}</a></li>\n"  
+                html_content += f"<li><a href=\"js/pdf/web/viewer.html?file=/src/subjects/{relative_path}\">{filename}</a></li>\n"
+            elif '.py' not in filename:
+                relative_path = os.path.relpath(os.path.join(dirpath, filename), root_dir)
+                html_content += f"<li><a href=\"./src/subjects/{relative_path}\">{filename}</a></li>\n"
                                                     # 结束HTML模板  
     html_content+= """
         </ul>
